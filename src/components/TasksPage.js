03d942dd44a69745417f6792e67a0a2eca0ae21f
sedx876 <swatkins876@gmail.com>
2020-09-04 10:51:19 -0400

    task form

--- src/components/TasksPage.js
+++ src/components/TasksPage.js
@@ -6,10 +6,24 @@ const TasksPage = () => {
       <div className="jumbotron py-3">
         <div className="row">
           <div className="col-md-2">
-            
+            <button className="btn btn-success m-3">+</button>
+          </div>
+          <div className="col-md-10">
+            <h2 className="display-4 text-center text-uppercase">Task Manager</h2>
           </div>
         </div>
-      </div>
+          {/*Input Form */}
+          <form>
+          <div className="form-group">
+          <input type="text" className="form-control" placeholder="Task Title" />
+          </div>
+          <div className="form-group">
+            <textarea type="text" className="form-control" placeholder="Task Description">
+            </textarea>
+          </div>
+          <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
+     </div>
     </div>
   )
 }
