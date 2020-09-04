7bc90155bdfce0f6668730de61491ed7545a4ed9
sedx876 <swatkins876@gmail.com>
2020-09-04 10:23:49 -0400

    task TaskList TaskPage boilerplate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Task from './components/Task'
+import TasksList from './components/TasksList'
+import TasksPage from './components/TasksPage'
 
 function App() {
   return (
     <div className="App">
       
+      <TasksPage />
     </div>
   );
 }
---
+++ src/components/TasksPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const TasksPage = () => {
+  return (
+    <div className="container my-5">
+      <div className="jumbotron py-3">
+        <div className="row">
+          <div className="col-md-2">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default TasksPage
